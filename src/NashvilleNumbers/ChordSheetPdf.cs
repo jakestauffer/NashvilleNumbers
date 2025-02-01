80c308195ffd3fa3fa83f23f1f931cdc78a0ff22
Jake Stauffer <stauffer23@gmail.com>
2025-02-01 12:11:02 -0500

    Improve chord identificaion and replacement.

--- src/NashvilleNumbers/ChordSheetPdf.cs
+++ src/NashvilleNumbers/ChordSheetPdf.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Text.RegularExpressions;
-using iText.Kernel.Pdf.Canvas.Parser.Listener;
-using iText.Kernel.Pdf.Canvas.Parser;
 using iText.Kernel.Pdf;
-using iText.Layout.Element;
-using iText.Layout;
+using iText.Kernel.Pdf.Canvas;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using iText.Kernel.Geom;
+using iText.Kernel.Font;
+using iText.IO.Font.Constants;
+using iText.Kernel.Pdf.Canvas.Parser.Data;
 
 namespace NashvilleNumbers
 {
@@ -25,50 +26,90 @@ namespace NashvilleNumbers
 
         public void ToNashvilleNumberPdf()
         {
-            var extractedText = ExtractTextFromPdf(_filePath);
-
-            var modifiedText = ReplaceChordsWithNashvilleNumbers(extractedText);
-
-            var fileName = Path.GetFileNameWithoutExtension(_filePath);
+            var fileName = System.IO.Path.GetFileNameWithoutExtension(_filePath);
             var outputFilePath = _filePath.Replace(fileName, $"{fileName} - Numbers");
 
-            SavePdf(modifiedText, outputFilePath);
-        }
-
-        private static string ExtractTextFromPdf(string pdfPath)
-        {
-            using PdfReader reader = new PdfReader(pdfPath);
-            using PdfDocument pdfDoc = new PdfDocument(reader);
-
-            var textWriter = new StringWriter();
+            using PdfReader reader = new PdfReader(_filePath);
+            using PdfWriter writer = new PdfWriter(outputFilePath);
+            using PdfDocument pdfDoc = new PdfDocument(reader, writer);
 
             for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
             {
-                textWriter.WriteLine(PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(i), new LocationTextExtractionStrategy()));
+                PdfPage page = pdfDoc.GetPage(i);
+                var chords = ExtractChordsFromPage(page);
+                ReplaceChordsOnPage(page, chords);
             }
 
-            return textWriter.ToString();
+            Console.WriteLine($"PDF saved: {outputFilePath}");
         }
 
-        private string ReplaceChordsWithNashvilleNumbers(string text)
+        private List<(string text, Rectangle rectangle, float fontSize, string fontStyle)> ExtractChordsFromPage(PdfPage page)
         {
-            string chordPattern = @"\b[A-G][#b]?(maj|min|m|dim|sus|aug)?\d*(\/[A-G][#b]?)?\b";
+            var chords = new List<(string text, Rectangle rectangle, float fontSize, string fontStyle)>();
 
-            return Regex.Replace(text, chordPattern, match =>
+            // Define regex pattern for valid chords (e.g., C, Am, Bb, D/F#)
+            string chordPattern = @"\b[A-G](#|b)?(maj|min|m|dim|aug|sus\d*|add\d*|7|9|11|13|6|b5|#5|b9|#9|#11|b13|no\d*)?(\/[A-G](#|b)?)?\b";
+
+            // Use iText's built-in regex strategy
+            var strategy = new RegexBasedLocationExtractionStrategy(new Regex(chordPattern));
+
+            PdfCanvasProcessor parser = new PdfCanvasProcessor(strategy);
+            parser.ProcessPageContent(page);
+
+
+            foreach (IPdfTextLocation match in strategy.GetResultantLocations())
             {
-                var chord = new Chord(_key, match.Value);
-                return chord.NashvilleNumber ?? chord.OriginalChord;
-            });
+                string text = match.GetText();
+                var rectangle = match.GetRectangle();
+                float fontSize = rectangle.GetHeight();
+                string fontStyle = "Helvetica";
+
+                chords.Add((text, rectangle, fontSize, fontStyle));
+            }
+
+            return chords;
+        }
+
+        private void ReplaceChordsOnPage(PdfPage page, List<(string text, Rectangle rectangle, float fontSize, string fontStyle)> chords)
+        {
+            PdfCanvas canvas = new PdfCanvas(page);
+
+            foreach (var (originalChord, rectangle, fontSize, fontStyle) in chords)
+            {
+                string nashvilleNumber = ConvertChordToNashville(originalChord);
+                if (nashvilleNumber == null) continue;
+
+                PdfFont font = PdfFontFactory.CreateFont(fontStyle);
+                float textWidth = font.GetWidth(nashvilleNumber, fontSize);
+                float textHeight = fontSize;
+
+                // Adjust erasure box size
+                var width = rectangle.GetWidth();
+                var height = rectangle.GetHeight();
+
+                float x = rectangle.GetX();
+                float y = rectangle.GetY();
+
+                canvas.SaveState();
+                canvas.SetFillColor(iText.Kernel.Colors.ColorConstants.WHITE);
+                canvas.Rectangle(rectangle);
+                canvas.Fill();
+                canvas.RestoreState();
+
+                canvas.BeginText();
+                canvas.SetFontAndSize(font, fontSize);
+                canvas.MoveText(x, y);
+                canvas.ShowText(nashvilleNumber);
+                canvas.EndText();
+            }
         }
 
 
-        static void SavePdf(string text, string outputPdfPath)
+        private string ConvertChordToNashville(string chord)
         {
-            using PdfWriter writer = new PdfWriter(outputPdfPath);
-            using PdfDocument pdfDoc = new PdfDocument(writer);
-
-            var document = new Document(pdfDoc);
-            document.Add(new Paragraph(text));
+            var nashvilleChord = new Chord(_key, chord);
+            return nashvilleChord.NashvilleNumber ?? chord;
         }
     }
-}
+ }
+
